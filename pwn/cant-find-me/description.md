86d0b0beeb6706284740e2b4d9ce398b9aa1f353
nordbo <eirik@nord.bo>
2020-11-09 21:11:27 +0100

    added fmtstr chall

---
+++ pwn/cant-find-me/description.md
@@ -0,0 +1,6 @@
+author: nordbo
+flag: KID20{1_h4v3_b33n_f0unD}
+
+--
+
+Can you find the flag?
---
+++ pwn/cant-find-me/files/cant-find-me.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+char flag[]  = "REDACTED";
+
+void ignore_me_init_buffering() {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void kill_on_timeout(int sig) {
+  if (sig == SIGALRM) {
+  	printf("[!] Anti DoS Signal. Patch me out for testing.");
+    _exit(0);
+  }
+}
+
+void ignore_me_init_signal() {
+	signal(SIGALRM, kill_on_timeout);
+	alarm(60);
+}
+
+int main() {
+    ignore_me_init_buffering();
+	ignore_me_init_signal();
+
+   char *flagPointer = flag;
+   char input[20];
+   puts("Where to look?");
+   fgets(input, 20, stdin);
+   printf(input);
+   return 0;
+}
