6b808f461d60a37395b2b4dfe40bb0f752fe3a19
Didrik Frimann Barroso Koren <dfkoren@studentmediene.no>
2020-11-09 23:18:29 +0100

    Use default base64 encoder

--- web/json_ham/app/main.py
+++ web/json_ham/app/main.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, redirect
 import orjson
-import codecs
+import base64
 
 app = Flask(__name__, static_url_path="/")
 
-# Check cookie for pickled user object, if changed, allow pwn
+# Check cookie for base64 encoded JSON user object, if show_flag is true, do it.
 @app.route('/')
 def hello_world():
     user = request.cookies.get('user')
     if user is None:
         return redirect('/login')
 
-    user = orjson.loads(codecs.decode(request.cookies.get('user').encode(), "base64"))
+    user = orjson.loads(base64.decodebytes(request.cookies.get('user').encode()))
     if user.get('show_flag'):
         return render_template('flag.html')
     return render_template('no_access.html', user=user)
 
-# Set pickled user object as cookie
+# Set base64 encoded JSON user object as cookie
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -24,11 +24,10 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         user = { 'username': username, 'password': password, 'show_flag': False }
-        base64_user = codecs.encode(orjson.dumps(user), "base64")
+        base64_user = base64.encodebytes(orjson.dumps(user))
         resp.set_cookie('user', base64_user)
         return resp
     return render_template('login.html')
 
 if __name__ == "__main__":
-    # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', port=80)
