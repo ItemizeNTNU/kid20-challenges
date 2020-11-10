1a42147a676627990ace64415e2cfd0e7cb6bdaf
Kristian <kristianint@outlook.com>
2020-11-10 16:48:34 +0100

    fix files and normalize name

--- misc/folder-search/solve.py
+++ misc/folder-search/solve.py
@@ -1,3 +1,4 @@
+
 import os
 
 files = os.listdir('files')
@@ -13,7 +14,10 @@ for f in range(0, int(len(files)), 2):
         t = d.read()
         d.close()
         tmp.append(t.encode("utf-8"))
-    
+
     byte = tmp[0] + tmp[1]
     flag += bytearray.fromhex(byte.decode("utf-8")).decode()
 print(flag)
+
+# Eventuelt
+# ls | sort -n | xargs cat | xxd -r -p
